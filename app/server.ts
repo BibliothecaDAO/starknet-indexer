e8e3e73f1478183cb4f4f5db1d292783168023f8
Ayooluwa Isaiah <ayisaiah@gmail.com>
2021-10-28 11:10:54 +0100

    Update packages to latest

--- app/server.ts
+++ app/server.ts
@@ -1,46 +1,47 @@
-import { ApolloServer } from "apollo-server-express";
-import Express from "express";
-import "reflect-metadata";
-import { buildSchema } from "type-graphql";
-import { connect } from "mongoose";
+import { ApolloServer } from 'apollo-server-express';
+import Express from 'express';
+import 'reflect-metadata';
+import { buildSchema } from 'type-graphql';
+import { connect } from 'mongoose';
 
-
-import {UserResolver} from "./resolvers/User";
-import {ProductResolver} from "./resolvers/Product";
-import {CategoriesResolver} from "./resolvers/Categories";
-import {CartResolver} from "./resolvers/Cart";
-import {OrderResolver} from "./resolvers/Order";
-
-
-
-
+import { UserResolver } from './resolvers/User';
+import { ProductResolver } from './resolvers/Product';
+import { CategoriesResolver } from './resolvers/Categories';
+import { CartResolver } from './resolvers/Cart';
+import { OrderResolver } from './resolvers/Order';
 
 const main = async () => {
-
-const schema = await buildSchema({
-    resolvers: [CategoriesResolver, ProductResolver, UserResolver, CartResolver, OrderResolver ],
+  const schema = await buildSchema({
+    resolvers: [
+      CategoriesResolver,
+      ProductResolver,
+      UserResolver,
+      CartResolver,
+      OrderResolver,
+    ],
     emitSchemaFile: true,
     validate: false,
   });
 
+  // create mongoose connection
+  const mongoose = await connect('mongodb://localhost:27017/test');
+  await mongoose.connection;
 
-// create mongoose connection
-const mongoose = await connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
-await mongoose.connection;
-  
+  const server = new ApolloServer({ schema });
 
+  const app = Express();
 
-const server = new ApolloServer({schema});
+  await server.start();
 
-const app = Express();
+  server.applyMiddleware({ app });
 
-server.applyMiddleware({app});
-
-app.listen({ port: 3333 }, () =>
-  console.log(`🚀 Server ready and listening at ==> http://localhost:3333${server.graphqlPath}`))
-
+  app.listen({ port: 3333 }, () =>
+    console.log(
+      `🚀 Server ready and listening at ==> http://localhost:3333${server.graphqlPath}`
+    )
+  );
 };
 
-main().catch((error)=>{
-    console.log(error, 'error');
-})
+main().catch((error) => {
+  console.log(error, 'error');
+});
