a472506dab3c86fd506a352000915ec2d46613d7
tenoxious <greg.choi+oxten@gmail.com>
2022-05-02 13:29:11 -0700

    fix: enable lore indexer

--- app/indexer/Starknet.ts
+++ app/indexer/Starknet.ts
@@ -1,16 +1,13 @@
 import { context } from "../context";
 import DesiegeIndexer from "./DesiegeIndexer";
-// import LoreIndexer from "./LoreIndexer";
+import LoreIndexer from "./LoreIndexer";
 import StarknetIndexer from "./StarknetIndexer";
 
 export const StarkNet = () => {
   return {
     async serverWillStart() {
       const indexer = new StarknetIndexer(
-        [
-          new DesiegeIndexer(context)
-          // new LoreIndexer(context)
-        ],
+        [new DesiegeIndexer(context), new LoreIndexer(context)],
         context
       );
       await indexer.start();
