df654c792656231761eecf2e7fabc549077cae67
tenoxious <tenox.0274@gmail.com>
2022-06-06 23:16:00 -0700

    fix: disable realms field resolvers

--- app/resolvers/settling/RealmResolver.ts
+++ app/resolvers/settling/RealmResolver.ts
@@ -3,81 +3,81 @@ import {
   Arg,
   Query,
   Ctx,
-  Int,
-  FieldResolver,
-  Root
+  Int
+  // FieldResolver,
+  // Root
 } from "type-graphql";
 import { Context } from "../../context";
 import {
   Realm,
   RealmHistory,
   CombatResult,
-  ResourceAmount,
-  Building,
-  Resource,
-  RealmTrait,
-  Troop
+  ResourceAmount
+  // Building,
+  // Resource,
+  // RealmTrait,
+  // Troop
 } from "../../entities";
 import { RealmInput } from "../types";
 import { RealmOrderByInput } from "../types/settling";
 import {
   RealmWhereInput,
-  RealmHistoryWhereInput,
-  RealmOrderByWithRelationInput
+  RealmHistoryWhereInput
+  // RealmOrderByWithRelationInput
 } from "@generated/type-graphql";
 
 @Resolver((_of) => Realm)
 export class RealmResolver {
-  @Query(() => [Realm])
-  async realms(
-    @Ctx() ctx: Context,
-    @Arg("filter", { nullable: true }) where: RealmWhereInput,
-    @Arg("orderBy", { nullable: true }) orderBy: RealmOrderByWithRelationInput,
-    @Arg("take", { nullable: true, defaultValue: 100 }) take: number,
-    @Arg("skip", { nullable: true, defaultValue: 0 }) skip: number
-  ) {
-    const data = await ctx.prisma.realm.findMany({
-      where,
-      orderBy,
-      take,
-      skip
-    });
-    return data;
-  }
+  // @Query(() => [Realm])
+  // async realms(
+  //   @Ctx() ctx: Context,
+  //   @Arg("filter", { nullable: true }) where: RealmWhereInput,
+  //   @Arg("orderBy", { nullable: true }) orderBy: RealmOrderByWithRelationInput,
+  //   @Arg("take", { nullable: true, defaultValue: 100 }) take: number,
+  //   @Arg("skip", { nullable: true, defaultValue: 0 }) skip: number
+  // ) {
+  //   const data = await ctx.prisma.realm.findMany({
+  //     where,
+  //     orderBy,
+  //     take,
+  //     skip
+  //   });
+  //   return data;
+  // }
 
-  @Query(() => Realm, { nullable: false })
-  async realm(@Ctx() ctx: Context, @Arg("id") realmId: number) {
-    return await ctx.prisma.realm.findUnique({
-      where: { realmId }
-    });
-  }
+  // @Query(() => Realm, { nullable: false })
+  // async realm(@Ctx() ctx: Context, @Arg("id") realmId: number) {
+  //   return await ctx.prisma.realm.findUnique({
+  //     where: { realmId }
+  //   });
+  // }
 
-  @FieldResolver(() => [Building])
-  async buildings(@Ctx() ctx: Context, @Root() realm: Realm) {
-    return await ctx.prisma.building.findMany({
-      where: { realmId: realm.realmId }
-    });
-  }
-  @FieldResolver(() => [Resource])
-  async resources(@Ctx() ctx: Context, @Root() realm: Realm) {
-    return await ctx.prisma.resource.findMany({
-      where: { realmId: realm.realmId }
-    });
-  }
+  // @FieldResolver(() => [Building])
+  // async buildings(@Ctx() ctx: Context, @Root() realm: Realm) {
+  //   return await ctx.prisma.building.findMany({
+  //     where: { realmId: realm.realmId }
+  //   });
+  // }
+  // @FieldResolver(() => [Resource])
+  // async resources(@Ctx() ctx: Context, @Root() realm: Realm) {
+  //   return await ctx.prisma.resource.findMany({
+  //     where: { realmId: realm.realmId }
+  //   });
+  // }
 
-  @FieldResolver(() => [RealmTrait])
-  async traits(@Ctx() ctx: Context, @Root() realm: Realm) {
-    return await ctx.prisma.realmTrait.findMany({
-      where: { realmId: realm.realmId }
-    });
-  }
+  // @FieldResolver(() => [RealmTrait])
+  // async traits(@Ctx() ctx: Context, @Root() realm: Realm) {
+  //   return await ctx.prisma.realmTrait.findMany({
+  //     where: { realmId: realm.realmId }
+  //   });
+  // }
 
-  @FieldResolver(() => [Troop])
-  async squad(@Ctx() ctx: Context, @Root() realm: Realm) {
-    return await ctx.prisma.troop.findMany({
-      where: { realmId: realm.realmId }
-    });
-  }
+  // @FieldResolver(() => [Troop])
+  // async squad(@Ctx() ctx: Context, @Root() realm: Realm) {
+  //   return await ctx.prisma.troop.findMany({
+  //     where: { realmId: realm.realmId }
+  //   });
+  // }
 
   @Query(() => Int)
   async realmsCount(
