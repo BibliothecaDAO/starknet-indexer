8a3eaa497024dec763f0bd44c14aac28a61a1233
tenoxious <tenox.0274@gmail.com>
2022-08-12 21:36:40 -0700

    fix: add unique realmId buildingId index on Building

--- app/indexer/RealmsBuildingIndexer.ts
+++ app/indexer/RealmsBuildingIndexer.ts
@@ -14,7 +14,7 @@ export default class RealmsBuildingIndexer extends BaseContractIndexer {
     this.on("BuildingIntegrity", this.buildingIntegrity.bind(this));
   }
 
- /* async buildBuilding(event: Event): Promise<void> {
+  /* async buildBuilding(event: Event): Promise<void> {
     const eventId = event.eventId;
     const params = event.parameters ?? [];
     const realmId = parseInt(params[0]);
@@ -57,10 +57,10 @@ export default class RealmsBuildingIndexer extends BaseContractIndexer {
     const params = event.parameters ?? [];
     const realmId = parseInt(params[0]);
     const buildingId = parseInt(params[2]);
-    const buildingIntegrityTimestamp = parseInt(params[3]) || 0
+    const buildingIntegrityTimestamp = parseInt(params[3]) || 0;
 
     const where = {
-      realmId_eventId: { realmId, eventId }
+      realmId_buildingId: { realmId, buildingId }
     };
     let builds: any[] = [];
     const building = await this.context.prisma.building.findUnique({ where });
@@ -74,8 +74,19 @@ export default class RealmsBuildingIndexer extends BaseContractIndexer {
     }
     await this.context.prisma.building.upsert({
       where,
-      create: { realmId, eventId, buildingIntegrity: buildingIntegrityTimestamp, buildingId, builds: [...builds] },
-      update: { buildingId, buildingIntegrity: buildingIntegrityTimestamp, builds: [...builds] }
+      create: {
+        realmId,
+        eventId,
+        buildingIntegrity: buildingIntegrityTimestamp,
+        buildingId,
+        builds: [...builds]
+      },
+      update: {
+        buildingId,
+        eventId,
+        buildingIntegrity: buildingIntegrityTimestamp,
+        builds: [...builds]
+      }
     });
     await this.saveRealmHistory({
       realmId,
