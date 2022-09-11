cc3e10be84dec30693ae9ec96cb651859f26a64b
tenoxious <tenox.0274@gmail.com>
2022-09-11 15:22:54 -0700

    fix: goblin typescript build errors

--- app/indexer/settling/GoblinIndexer.ts
+++ app/indexer/settling/GoblinIndexer.ts
@@ -1,9 +1,9 @@
 import { Event } from "./../../entities/starknet/Event";
 import { Context } from "./../../context";
 import BaseContractIndexer from "./../BaseContractIndexer";
-import { uint256ToBN } from "starknet/utils/uint256";
-import { BigNumberish } from "starknet/utils/number";
-import { GOBLIN_SQUAD_SLOT } from "../../utils/game_constants";
+// import { uint256ToBN } from "starknet/utils/uint256";
+// import { BigNumberish } from "starknet/utils/number";
+// import { GOBLIN_SQUAD_SLOT } from "../../utils/game_constants";
 // import CombatIndexer, {
 //   SQUAD_LENGTH,
 //   SQUAD_ATTRIBUTES_LENGTH
@@ -12,9 +12,9 @@ import { GOBLIN_SQUAD_SLOT } from "../../utils/game_constants";
 const CONTRACT =
   "0x03f233b0c1aa84b4a05ff55c7a3a0c1f6ba876030b06200ac27e80f338f59120";
 
-function arrayUInt256ToNumber([low, high]: any[]): BigNumberish {
-  return parseInt(uint256ToBN({ low, high }).toString());
-}
+// function arrayUInt256ToNumber([low, high]: any[]): BigNumberish {
+//   return parseInt(uint256ToBN({ low, high }).toString());
+// }
 
 //Troop(id=1, type=TroopType.Melee, tier=1, building=1, agility=1, attack=1, armor=3, vitality=4, wisdom=1),
 
@@ -25,7 +25,7 @@ export default class GoblinIndexer extends BaseContractIndexer {
     this.on("GoblinSpawn", this.goblinSpawn.bind(this));
   }
 
-  async goblinSpawn(event: Event) {
+  async goblinSpawn(_event: Event) {
     // const params = event.parameters ?? [];
     // const realmId = arrayUInt256ToNumber(params.slice(0, 2));
     // const squad = params.slice(2, SQUAD_ATTRIBUTES_LENGTH * SQUAD_LENGTH + 2);
